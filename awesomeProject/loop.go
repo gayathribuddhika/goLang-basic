b3d1993dd8a1734baa3de62916ac00a765ca91ee
Gayathri Buddhika <39160904+gayathribuddhika@users.noreply.github.com>
2021-01-17 23:39:37 +0530

    Added new for loop for this file

--- awesomeProject/loop.go
+++ awesomeProject/loop.go
@@ -3,11 +3,11 @@ package main
 import "fmt"
 
 func main () {
-	//x :=1
-	//for x<=5 {
-	//	fmt.Println(x)
-	//	x++
-	//}
+	x :=1
+	for x<=5 {
+		fmt.Println(x)
+		x++
+	}
 
 	for i :=1; i<=5; i++ {
 		fmt.Println("Gayathri ", i)
