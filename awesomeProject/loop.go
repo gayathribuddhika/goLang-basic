ffb3aad9eb408c5bfa1f45a1715eedda7aa8e619
Gayathri Buddhika <39160904+gayathribuddhika@users.noreply.github.com>
2021-01-17 23:38:19 +0530

    Add files via upload

--- awesomeProject/loop.go
+++ awesomeProject/loop.go
@@ -3,13 +3,28 @@ package main
 import "fmt"
 
 func main () {
-	//i :=1
-	//for i<=5 {
-	//	fmt.Println(i)
-	//	i++
+	//x :=1
+	//for x<=5 {
+	//	fmt.Println(x)
+	//	x++
 	//}
 
 	for i :=1; i<=5; i++ {
 		fmt.Println("Gayathri ", i)
+	}
+
+	for x := 0; x<100; x++ {
+		if x!= 0 && x%3 == 0 && x%7 == 0 {
+			fmt.Println(x)
+			continue  // print all the numbers that condition true
+		}
+	}
+	fmt.Println(" ")
+
+	for x := 0; x<100; x++ {
+		if x!= 0 && x%3 == 0 && x%7 == 0 {
+			fmt.Println(x)
+			break // print only the first number that condition true
+		}
 	}
 }
