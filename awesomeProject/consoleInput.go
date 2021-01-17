182ff44de3b65b4716c2b42a72f34d6ccf929ad8
Gayathri Buddhika <39160904+gayathribuddhika@users.noreply.github.com>
2021-01-17 23:54:29 +0530

    Separate the things

--- awesomeProject/consoleInput.go
+++ awesomeProject/consoleInput.go
@@ -11,9 +11,11 @@ func  main()  {
 	fmt.Println("Enter your name: ")
 	scanner.Scan() 							// default the value that we entered get as a string here
 	input1 := scanner.Text()
+	
 	fmt.Println("Enter your age: ")
 	scanner.Scan() 							// default the value that we entered get as a string here
 	input2, _ := strconv.ParseInt(scanner.Text(), 10, 64)
+	
 	fmt.Printf ("You entered your name as %q ", input1)
 	fmt.Println()
 	fmt.Printf ("Your age is %d ", input2)
